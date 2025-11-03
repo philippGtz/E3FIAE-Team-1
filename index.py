17ac7cfd437777a4b947cdae600292da639be155
Emre Cevik <emre.cevik@sap.com>
2025-11-03 11:34:08 +0100

    feat: Add Catalog Screen and sample catalogs

--- index.py
+++ index.py
@@ -5,3 +5,7 @@ bp_index = Blueprint('index', __name__)
 @bp_index.route('/')
 def index():
     return render_template('index.html')
+
+@bp_index.route('/catalog')
+def catalog():
+    return render_template('catalog.html')
