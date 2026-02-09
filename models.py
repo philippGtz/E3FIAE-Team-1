10181edf7aedb3728dff0d328ee4e86c0a76327e
joris <joris@unkel-net.de>
2026-02-09 14:24:33 +0100

    Sprint #1 geadded

--- models.py
+++ models.py
@@ -16,23 +16,21 @@ class Users(db.Model):
     __tablename__ = "users"
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    phone_number = db.Column(db.String(20), unique=True, nullable=True)
+    phone_number = db.Column(db.Integer, unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-    first_name = db.Column(db.String(120), nullable=True)
-    last_name = db.Column(db.String(120), nullable=True)
+    username = db.Column(db.String(120), nullable=True)
     address = db.Column(db.String(120), unique=False, nullable=False)
-    house_number = db.Column(db.String(10), nullable=True)
-    postal_code = db.Column(db.String(10), nullable=True)
-    city = db.Column(db.String(120), nullable=True)
-    country = db.Column(db.String(120), nullable=True)
-    iban = db.Column(db.String(120), unique=True, nullable=True)
+    iban = db.Column(db.String(120), unique=True, nullable=False)
 
 class Orders(db.Model):
     __tablename__ = "orders"
     order_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    bc_id = db.Column(db.Integer, db.ForeignKey('bike_computers.bc_id'), nullable=False)
+    bike_computer_id = db.Column(db.Integer, db.ForeignKey('bike_computers.bc_id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
+    sap_terminnummer = db.Column(db.Integer, nullable=False)
+    order_status = db.Column(db.String(120), nullable=False)
+    delivery_date = db.Column(db.Date, nullable=False)
     
     @property
     def id(self):
